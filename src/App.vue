413dde3870b776e5297c9d7072f07a000f7c1841
xsj <xsj20@outlook.com>
2022-07-08 20:56:19 +0800

    优化结构

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
   <div id="app">
-   
+    <Avatar url="http://mdrs.yuanjin.tech/img/20201031141350.jpg" />
+    <Icon type="arrowDown" />
+    <Pager
+      :total="total"
+      :current="current"
+      @pageChange="handlePageChange($event)"
+    />
   </div>
 </template>
 
 <script>
+import Avatar from "./components/Avatar";
+import Icon from "./components/Icon";
+import Pager from "./components/Pager";
 export default {
   name: "App",
   components: {
-  }
+    Avatar,
+    Icon,
+    Pager,
+  },
+  data() {
+    return {
+      total: 301,
+      current: 1,
+    };
+  },
+  methods: {
+    handlePageChange(newPage) {
+      this.current = newPage;
+      console.log("加载当前页数据");
+    },
+  },
 };
 </script>
 
